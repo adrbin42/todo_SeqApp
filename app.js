c2ed88e4e1cc73149339859647f848adfaa2abc8
adrienne <adriennebing@gmail.com>
2017-06-28 15:31:55 -0400

    All basic setup complete, home page renders

--- app.js
+++ app.js
@@ -4,24 +4,26 @@ const mustacheExpress = require('mustache-express');
 const bodyParser = require("body-parser");
 const expressValidator = require("express-validator");
 const morgan = require("morgan");
-const routes = require("./routes");
+const routes = require("./routes/router");
+// Word Router
+const appRouter = require('./routes/router');
 
 const app = express();
 
 app.engine('mustache', mustacheExpress());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'mustache')
-app.use('/static', express.static('static'));
+app.use('/public', express.static('public'));
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(expressValidator());
 app.use(morgan('dev'))
 
-app.use(routes);
+app.use('/router',appRouter);
 
 app.get("/",function(req,res){
   console.log("Everything works");
-  res.render("Page works");
+  res.render("index");
 });
 
 app.listen(3000, function () {
---
+++ routes/router.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+
+const word = require('../models/todo_list.js');
+
+module.exports = router;
